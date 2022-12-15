1cdd913b0454c57270812fcd8af2412245f8e029
Steve Zaccardi <steve@zaccardi.org>
2022-12-15 16:44:59 -0500

    First commit.

--- WebTools/src/pages/selectionPage.tsx
+++ WebTools/src/pages/selectionPage.tsx
@@ -20,7 +20,7 @@ export class SelectionPage extends React.Component<IPageProperties, {}> {
         return (
             <div className="page">
                 <div className="page-text">
-                    Select tool.
+                    Test tool.
                 </div>
                 <div className="button-container">
                     <Button text="Characters & Starships" className="button" onClick={() => { this.selectTool(Tool.CharacterGenerator); }} />
